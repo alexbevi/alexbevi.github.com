25aaf24b97456616abbb50c4942de9bad92730eb
Alex Bevilacqua <alexbevi@gmail.com>
2012-07-18 06:07:44 -0400

    initial checkin

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/alexbevi/"
+http_images_path = "/alexbevi/images"
+http_fonts_path = "/alexbevi/fonts"
+css_dir = "public/alexbevi/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
