09fcfae5a2f04fe087ec9534441b75798502f3c8
Alex Bevilacqua <alexbevi@gmail.com>
2014-04-21 08:27:20 -0400

    Revert "updated google-form"
    This reverts commit 205292f348ccb658ba6e5b054d32d42b157d4474.

--- plugins/google_form.rb
+++ plugins/google_form.rb
@@ -1,5 +1,5 @@
 # Title: Google Form
-# Authors: Arshad Chummun
+# Authors: Arshad http://donothackcore.com
 # Description: A filter to embed Google Forms in Octopress
 #
 # Syntax {% google_form formkey [message] %}
@@ -19,17 +19,17 @@ module Jekyll
     @formhtml = ''
 
     def initialize(tag_name, markup, tokens)
-      if markup =~ /([a-zA-Z0-9_\-]*)?\s(.+)/
+      if markup =~ /([a-zA-Z0-9]*)?\s(.+)/
         @formkey = $1
         @message = $2
-
+        
         #url of the Google Form
-        @url = "https://docs.google.com/forms/d/#{@formkey}/viewform?embedded=true"
-
+        @url = "https://docs.google.com/spreadsheet/embeddedform?formkey=#{@formkey}"
+        
         #parse the HTML and get the form markup only
         doc = Nokogiri::HTML(open(@url))
         form = doc.xpath("//form").first.unlink
-
+        
         @formhtml = form.to_html
       end
       super
