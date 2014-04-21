205292f348ccb658ba6e5b054d32d42b157d4474
Alex Bevilacqua <alexbevi@gmail.com>
2014-04-21 08:01:20 -0400

    updated google-form

--- plugins/google_form.rb
+++ plugins/google_form.rb
@@ -1,5 +1,5 @@
 # Title: Google Form
-# Authors: Arshad http://donothackcore.com
+# Authors: Arshad Chummun
 # Description: A filter to embed Google Forms in Octopress
 #
 # Syntax {% google_form formkey [message] %}
@@ -19,17 +19,17 @@ module Jekyll
     @formhtml = ''
 
     def initialize(tag_name, markup, tokens)
-      if markup =~ /([a-zA-Z0-9]*)?\s(.+)/
+      if markup =~ /([a-zA-Z0-9_\-]*)?\s(.+)/
         @formkey = $1
         @message = $2
-        
+
         #url of the Google Form
-        @url = "https://docs.google.com/spreadsheet/embeddedform?formkey=#{@formkey}"
-        
+        @url = "https://docs.google.com/forms/d/#{@formkey}/viewform?embedded=true"
+
         #parse the HTML and get the form markup only
         doc = Nokogiri::HTML(open(@url))
         form = doc.xpath("//form").first.unlink
-        
+
         @formhtml = form.to_html
       end
       super
