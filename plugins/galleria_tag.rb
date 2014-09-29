c0ac091c8abfe3ece2fb21c5a01c238047c6a3f5
Alex Bevilacqua <alex@alexbevi.com>
2014-09-29 08:47:02 -0400

    updated

---
+++ plugins/galleria_tag.rb
@@ -0,0 +1,63 @@
+module Jekyll
+
+  class PhotosUtil
+    def initialize(context)
+      @context = context
+    end
+
+    def path_for(filename)
+      filename = filename.strip
+      prefix = (@context.environments.first['site']['photos_prefix'] unless filename =~ /^(?:\/|http)/i) || ""
+      "#{prefix}#{filename}"
+    end
+  end
+
+  class GalleriaScriptIncludePatch < Liquid::Tag
+    def render(context)
+      return <<-eof
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/galleria.min.js" type="text/javascript"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/themes/classic/galleria.classic.min.js" type="text/javascript"></script>
+<link href="//cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/themes/classic/galleria.classic.min.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<style>
+  /* This rule is read by Galleria to define the gallery height: */
+  #galleria{height:320px}
+</style>
+      eof
+    end
+  end
+
+  class GalleriaTag < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      # No initializing needed
+      super
+    end
+
+    def render(context)
+      # Convert the entire content array into one large string
+      lines = super
+      # split the text by newlines
+      lines = lines.split("\n")
+      
+      p = PhotosUtil.new(context)
+      gallery = "<div id=\"galleria\">"
+
+      lines.each_with_index do |line, i|
+        next if line.empty?
+        filename, title = line.split(":")
+        title = (title.nil?) ? "" : title.strip
+        gallery << "<a href=<img src=\"#{p.path_for(filename)}\" data-title=\"#{title}\" />"
+      end
+      gallery << "</div>"
+      gallery << "<script>"
+      #gallery << "  Galleria.configure('transition', 'fade');"
+      #gallery << "  Galleria.run('#galleria');"
+      gallery << "</script>"
+      gallery
+    end
+  end
+
+end
+
+Liquid::Template.register_tag('galleria', Jekyll::GalleriaTag)
+Liquid::Template.register_tag('galleria_includes', Jekyll::GalleriaScriptIncludePatch)
---
+++ source/_posts/2014-09-26-just-finished-robotrek.markdown
@@ -0,0 +1,48 @@
+---
+layout: post
+title: "Just Finished - Robotrek"
+date: 2014-09-26 08:13:53 -0400
+comments: true
+categories: [just-finished, rpg, snes]
+---
+
+{% galleria %}
+/images/robotrek/Robotrek_(USA).000.png
+/images/robotrek/Robotrek_(USA).001.png
+/images/robotrek/Robotrek_(USA).002.png
+/images/robotrek/Robotrek_(USA).003.png
+/images/robotrek/Robotrek_(USA).004.png
+/images/robotrek/Robotrek_(USA).005.png
+/images/robotrek/Robotrek_(USA).006.png
+/images/robotrek/Robotrek_(USA).007.png
+/images/robotrek/Robotrek_(USA).008.png
+/images/robotrek/Robotrek_(USA).009.png
+/images/robotrek/Robotrek_(USA).010.png
+/images/robotrek/Robotrek_(USA).011.png
+/images/robotrek/Robotrek_(USA).012.png
+/images/robotrek/Robotrek_(USA).013.png
+/images/robotrek/Robotrek_(USA).014.png
+/images/robotrek/Robotrek_(USA).015.png
+/images/robotrek/Robotrek_(USA).016.png
+/images/robotrek/Robotrek_(USA).017.png
+/images/robotrek/Robotrek_(USA).018.png
+/images/robotrek/Robotrek_(USA).019.png
+/images/robotrek/Robotrek_(USA).020.png
+/images/robotrek/Robotrek_(USA).021.png
+/images/robotrek/Robotrek_(USA).022.png
+/images/robotrek/Robotrek_(USA).023.png
+/images/robotrek/Robotrek_(USA).024.png
+/images/robotrek/Robotrek_(USA).025.png
+/images/robotrek/Robotrek_(USA).026.png
+/images/robotrek/Robotrek_(USA).027.png
+/images/robotrek/Robotrek_(USA).028.png
+/images/robotrek/Robotrek_(USA).029.png
+/images/robotrek/Robotrek_(USA).030.png
+/images/robotrek/Robotrek_(USA).031.png
+/images/robotrek/Robotrek_(USA).032.png
+/images/robotrek/Robotrek_(USA).033.png
+/images/robotrek/Robotrek_(USA).034.png
+/images/robotrek/Robotrek_(USA).035.png
+/images/robotrek/Robotrek_(USA).036.png
+/images/robotrek/Robotrek_(USA).037.png
+{% endgalleria %}
