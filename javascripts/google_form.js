2d2eafc6e3c940654340154a5fcd56eb19e9ecdc
Alex Bevilacqua <alex@alexbevi.com>
2020-01-17 09:19:17 -0500

    Site updated at 2020-01-17 14:19:16 UTC

--- javascripts/google_form.js
+++ javascripts/google_form.js
@@ -1,40 +1,33 @@
-(function ($) {
-  $(document).ready(function() {
-    //clean up empty tags
-    $('.google-form-wrapper p, .google-form-wrapper br')
-      .filter(function() {
-        return $(this).html() == '';
-      })
-      .remove();
-
-    //add required class to required elements
-    $('.google-form-wrapper form')
-      .find('.ss-item-required input, .ss-item-required textarea')
-      .filter(function() {
-        return jQuery(this).attr('name').match(/entry\.\d\.single/);
-      })
-      .addClass('required');
-
-    //validate the form
-    $('.google-form-wrapper form').validate({
-      submitHandler: function(form) {
-        $(form)
-          .ajaxSubmit({
-            success: function(data) {
-              if (data) {
-                $(form)
-                .hide(200, function() {
-                  $(this)
-                    .prev('.success-msg')
-                    .fadeIn('slow')
-                })
-              }
-            },
-            error : function (data) {
-              console.error(data);
-            }
-          })
-      }
-    });
+jQuery(document).ready(function() {
+  //clean up empty tags
+  jQuery('.google-form-wrapper p, .google-form-wrapper br')
+    .filter(function() {
+      return jQuery(this).html() == '';
+    })
+    .remove();
+  
+  //add required class to required elements
+  jQuery('.google-form-wrapper form')
+    .find('.ss-item-required input, .ss-item-required textarea')
+    .filter(function() {
+      return jQuery(this).attr('name').match(/entry\.\d\.single/);
+    })
+    .addClass('required');
+ 
+  //validate the form
+  jQuery('.google-form-wrapper form').validate({
+    submitHandler: function(form) {
+      jQuery(form)
+        .ajaxSubmit({
+          error : function (data) { //This is strangely working on error only
+            jQuery(form)
+              .hide(200, function() {
+                jQuery(this)
+                  .prev('.success-msg')
+                  .fadeIn('slow')
+              })
+          }
+        })
+    }
   });
-})(jQuery);
+});
