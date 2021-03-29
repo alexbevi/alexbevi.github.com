b2dba43efab95331b6bf76ea5e966901b73f6afc
Alex Bevilacqua <alex@alexbevi.com>
2021-03-29 14:54:25 -0400

    Site updated at 2021-03-29 18:54:25 UTC

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-/* Registering Service Worker */
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
-};
-
+/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/sw.js'); };
